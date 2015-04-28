d2467eb5d2f71c28ac9dd1632e5b1cea79dafb4b
agust220 <agusth14@ru.is>
2015-04-28 16:27:29 +0000

    Fyrsta commit, empty project

---
+++ SocialNet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SocialNet.Startup))]
+namespace SocialNet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
